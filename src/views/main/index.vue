54636ea4ecbf87f57fe37fa39888c9d37765c034
zhangfei <zhangfei@wisecotech.com>
2021-01-22 17:10:38 +0800

    init

---
+++ src/views/main/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app-container">
+    main
+  </div>
+</template>
+
+<script>
+import { list } from '@/api/user'
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+    
+  },
+  created() {
+    list().then(res => res)
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
